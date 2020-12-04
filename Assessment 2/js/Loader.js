4d4c14bd18eba22c8e8cc60b8c744514ffa47009
basarinan <basar.inan@ucc.on.ca>
2020-12-04 22:26:10 +0100

    Commit

---
+++ Assessment 2/js/Loader.js
@@ -0,0 +1,16 @@
+ 
+ document.addEventListener('DOMContentLoaded',function(){
+     var elems = document.querySelectorAll('.modal');
+     var instances = M.Modal.init($("#modaltest"),inDuration(1000))
+ })
+ 
+ $(document).ready(function(){
+    $('.modal').modal();
+
+    
+    
+    
+
+}) 
+
+
